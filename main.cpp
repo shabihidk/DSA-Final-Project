d3840610c5231c0c948d89eaa5cb8f25b4050edf
Muhammad Shabih Ul Hassan Raja <165433929+shabihidk@users.noreply.github.com>
2024-12-08 01:15:24 +0500

    Test02 : Menu Setup for Maze Game

--- main.cpp
+++ main.cpp
@@ -129,6 +129,7 @@ class Linkedlist{
     };
 };
 
+//QUEUE
 class Queue 
 {
     private:
@@ -221,7 +222,7 @@ class Queue
 
 
 
-
+// BST
 struct NodeT
 {
     float time;
@@ -281,6 +282,7 @@ class BinarySearchTree{
         }
 };
 
+// LEADERBOARD FUNCTIONS
 
 void LeaderboardDisplay(){
     ifstream file("leaderboard.txt");
@@ -323,6 +325,8 @@ void LeaderboardDisplay(){
             break;
     }
 }
+
+// STACK
 
 class stack {
     private:
@@ -378,179 +382,227 @@ class stack {
 
 };
 
-class Search {
-private:
-    vector<vector<int>> maze;
-    stack s;
-    int startX, startY, endX, endY;
-    bool searchComplete;
-    string statusMessage;
-
-public:
-    Search(vector<vector<int>> m) : maze(m), searchComplete(false){
-        for (int i = 0; i < SIZE; ++i) {
-            for (int j = 0; j < SIZE; ++j) {
-                if (maze[i][j] == 4) {
-                    startX = i;
-                    startY = j;
-                }
-                if (maze[i][j] == 9) {
-                    endX = i;
-                    endY = j;
-                }
-            }
-        }
-        s.push({startX,startY});
-
-    }
-
-    void searchstep() 
-    {
-        if (s.empty())
-        {
-            searchComplete = true;
-            cout << "Exit Not Found. \n";
-            return; 
-        }   
-        while(!s.empty())
-        {
-            int x;
-            x = s.topElement().first;
-
-            int y = s.topElement().second;
-
-            s.pop();
-
-            if (x == endX && y == endY)
-            {
-                searchComplete = true;
-                statusMessage = "Exit found at (" + to_string(x) + ", " + to_string(y) + ")";
-                return;
-            };
-
-            maze[x][y] = 5;
-
-            if ( x-1 >= 0 && maze[x - 1][y] == 1) 
-            {
-                s.push({x -1, y});
-            };
-            if (x + 1 < SIZE && maze[x + 1][y] == 1) 
-            {
-                s.push({x + 1, y});
-            };
-            if (y - 1 >= 0 && maze[x][y - 1] == 1) 
-            {
-                s.push({x, y - 1});
-            };
-            if (y + 1 < SIZE && maze[x][y + 1] == 1) 
-            {
-                s.push({x, y + 1});
-            };
-
-        };
-
-    };
-
-    bool isSearchComplete()
-    {
-        return searchComplete;
-    }
-
-    void DrawMaze()
-    {
-        BeginDrawing();
-        ClearBackground(RAYWHITE);
-
-        for (int i = 0; i < SIZE; ++i)
-        {
-            for (int j = 0; j < SIZE; ++j)
-            {
-                Color color;
-                switch (maze[i][j])
-                {   
-                    case 1: color = DARKGRAY; break;
-                    case 4: color = GREEN; break;
-                    case 9: color = RED; break;
-                    case 5: color = BLUE; break;
-                    default: color = LIGHTGRAY; break; 
-
-                }
-
-                DrawRectangle(j * TILE_SIZE, i * TILE_SIZE, TILE_SIZE, TILE_SIZE, color);
-
-            }
-        }
-
-        DrawText(statusMessage.c_str(), 10, SIZE * TILE_SIZE + 10, 20, DARKGRAY);
-
-        EndDrawing();
-    }
-    
-};
+// MAZE GAME FUNCTIONS - Shabih
 
 struct Player
 {
-    int x, y;
+    string name;
+    float time;
 };
 
-Player player = {1, 1};
+int maze[MAZE_HEIGHT][MAZE_WIDTH];
 
 
-vector<vector<int>> maze = {
-    {1, 1, 1, 1, 1},
-    {1, 5, 5, 5, 1},
-    {1, 5, 1, 9, 1},
-    {1, 1, 1, 5, 1},
-    {4, 5, 1, 1, 1}
+void MapDefine()
+{
+    ifstream file("map.txt");
+
+    if (!file) {
+        cerr << "Error opening file!" << endl;
+        return;
+    }
+
+    for (int i = 0; i < MAZE_HEIGHT; i++) {
+        for (int j = 0; j < MAZE_WIDTH; j++) {
+            file >> maze[i][j];
+        }
+    }
+
+    file.close();
 };
 
+//  ---------------------- CODE NOT WORKING [ segmentation error ]---------------------- 
 
-//int playerX = 1, playerY = 1;
-//int goalX = SIZE - 2, goalY = SIZE - 2;
+// class Search {
+// private:
+//     vector<vector<int>> maze;
+//     stack s;
+//     int startX, startY, endX, endY;
+//     bool searchComplete;
+//     string statusMessage;
+
+// public:
+//     Search(vector<vector<int>> m) : maze(m), searchComplete(false){
+//         for (int i = 0; i < SIZE; ++i) {
+//             for (int j = 0; j < SIZE; ++j) {
+//                 if (maze[i][j] == 4) {
+//                     startX = i;
+//                     startY = j;
+//                 }
+//                 if (maze[i][j] == 9) {
+//                     endX = i;
+//                     endY = j;
+//                 }
+//             }
+//         }
+//         s.push({startX,startY});
+
+//     }
+
+//     void searchstep() 
+//     {
+//         if (s.empty())
+//         {
+//             searchComplete = true;
+//             cout << "Exit Not Found. \n";
+//             return; 
+//         }   
+//         while(!s.empty())
+//         {
+//             int x;
+//             x = s.topElement().first;
+
+//             int y = s.topElement().second;
+
+//             s.pop();
+
+//             if (x == endX && y == endY)
+//             {
+//                 searchComplete = true;
+//                 statusMessage = "Exit found at (" + to_string(x) + ", " + to_string(y) + ")";
+//                 return;
+//             };
+
+//             maze[x][y] = 5;
+
+//             if ( x-1 >= 0 && maze[x - 1][y] == 1) 
+//             {
+//                 s.push({x -1, y});
+//             };
+//             if (x + 1 < SIZE && maze[x + 1][y] == 1) 
+//             {
+//                 s.push({x + 1, y});
+//             };
+//             if (y - 1 >= 0 && maze[x][y - 1] == 1) 
+//             {
+//                 s.push({x, y - 1});
+//             };
+//             if (y + 1 < SIZE && maze[x][y + 1] == 1) 
+//             {
+//                 s.push({x, y + 1});
+//             };
+
+//         };
+
+//     };
+
+//     bool isSearchComplete()
+//     {
+//         return searchComplete;
+//     }
+
+    // void DrawMaze()
+    // {
+    //     BeginDrawing();
+    //     ClearBackground(RAYWHITE);
+
+    //     for (int i = 0; i < SIZE; ++i)
+    //     {
+    //         for (int j = 0; j < SIZE; ++j)
+    //         {
+    //             Color color;
+    //             switch (maze[i][j])
+    //             {   
+    //                 case 1: color = DARKGRAY; break;
+    //                 case 4: color = GREEN; break;
+    //                 case 9: color = RED; break;
+    //                 case 5: color = BLUE; break;
+    //                 default: color = LIGHTGRAY; break; 
+
+    //             }
+
+    //             DrawRectangle(j * TILE_SIZE, i * TILE_SIZE, TILE_SIZE, TILE_SIZE, color);
+
+    //         }
+    //     }
+
+    //     DrawText(statusMessage.c_str(), 10, SIZE * TILE_SIZE + 10, 20, DARKGRAY);
+
+    //     EndDrawing();
+    // };
+    
+
+int playerX = 1, playerY = 1;
+int goalX = MAZE_WIDTH - 2, goalY = MAZE_HEIGHT - 2;
+vector<Player> leaderboard;
 float timer = 0.0f;
 bool gameWon = false;
 bool startScreen = true;
 char playerName[20] = "";
+bool showingLeaderboard = false;
 
+void ShowLeaderboard();
+void SaveLeaderboard();
+void LoadLeaderboard();
 
+void DrawMaze();
 void HandlePlayerMovement();
 void DrawGameInfo();
 void ResetGame();
 
 int main() {
     
-    InitWindow(SIZE * TILE_SIZE, SIZE * TILE_SIZE, "Maze Search Visualization");
-    SetTargetFPS(10);
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Maze Search Visualization");
+    SetTargetFPS(50);
+
+    MapDefine();
+    
+    LoadLeaderboard();
 
 
-    vector<vector<int>> maze =
-    { 
-    {1, 1, 1, 1, 1}, 
-    {1, 5, 5, 5, 1},
-    {1, 5, 1, 9, 1},
-    {1, 1, 1, 5, 1},
-    {4, 5, 1, 1, 1}
-    };
-
-
-Search search(maze);
-
-
-while(!WindowShouldClose())
-{ BeginDrawing();
+    while(!WindowShouldClose())
+    {   
+        BeginDrawing();
         ClearBackground(BACKGROUND_COLOR);
 
         if (startScreen) {
             DrawText("MAZE GAME", SCREEN_WIDTH / 2 - 100, SCREEN_HEIGHT / 2 - 50, 40, TEXT_COLOR);
             DrawText("Press ENTER to Start", SCREEN_WIDTH / 2 - 120, SCREEN_HEIGHT / 2, 20, TEXT_COLOR);
             if (IsKeyPressed(KEY_ENTER)) startScreen = false;
+        } else if(showingLeaderboard) 
+        {
+            ShowLeaderboard();
+            if(IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+            {
+                Vector2 mousePos = GetMousePosition();
+                if (mousePos.x > SCREEN_WIDTH / 2 - 100 && mousePos.x < SCREEN_WIDTH / 2 + 100 &&
+                    mousePos.y > SCREEN_HEIGHT - 50 && mousePos.y < SCREEN_HEIGHT - 10) {
+                    ResetGame();
+                }
+            };
+        }
+
+        else if (gameWon)
+        {
+        DrawRectangle(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, WIN_SCREEN_COLOR);
+            DrawText("YOU WON!", SCREEN_WIDTH / 2 - 100, SCREEN_HEIGHT / 2 - 50, 30, WHITE);
+            DrawText("Enter your name:", SCREEN_WIDTH / 2 - 100, SCREEN_HEIGHT / 2, 20, WHITE);
+
+            int key = GetCharPressed();
+            if (key > 0 && strlen(playerName) < 19) playerName[strlen(playerName)] = (char)key;
+            if (IsKeyPressed(KEY_BACKSPACE) && strlen(playerName) > 0) playerName[strlen(playerName) - 1] = '\0';
+
+            if (IsKeyPressed(KEY_ENTER)) {
+                Player newPlayer;
+                newPlayer.name = playerName;
+                newPlayer.time = timer;
+                leaderboard.push_back(newPlayer);
+
+                sort(leaderboard.begin(), leaderboard.end(), [](const Player &a, const Player &b) {
+                    return a.time < b.time;
+                });
+
+                SaveLeaderboard();
+                showingLeaderboard = true;
+            }
+            DrawText(playerName, SCREEN_WIDTH / 2 - 100, SCREEN_HEIGHT / 2 + 30, 20, YELLOW);
         } else {
             timer += GetFrameTime();
-            HandlePlayerMovement();
-            search.searchstep();
-            search.DrawMaze();
-            DrawGameInfo();
+            HandlePlayerMovement();  
+            DrawMaze();
+            DrawText(TextFormat("Time: %.2f seconds", timer), 10, 10, 20, TEXT_COLOR);
         }
+        
    EndDrawing();
    };
 
